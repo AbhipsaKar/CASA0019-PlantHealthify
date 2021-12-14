9fabb984e569644c92908ac4c3b916d3fc0a8771
AbhipsaKar <91799774+AbhipsaKar@users.noreply.github.com>
2021-12-14 19:51:23 +0000

    added new prefab for personalised gauge and created the lcd display

--- Digiverse/AR_PlantHealthify/Assets/AR_Gauge/Scripts/mqttController.cs
+++ Digiverse/AR_PlantHealthify/Assets/AR_Gauge/Scripts/mqttController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 public class mqttController : MonoBehaviour
 {
     public string nameController = "Controller";
@@ -29,7 +30,7 @@ public class mqttController : MonoBehaviour
     {
         Debug.Log("Event Fired. The message, from Object " + nameController + " is = " + mqttObject.msg);
         string tempMoistureTopic = topic.Substring(0,topic.Length-1);
-        tempMoistureTopic += "moisture";
+        tempMoistureTopic += "moisture";  //Check for moisture
         Debug.Log("Temp topic ="+tempMoistureTopic);
         Debug.Log("topic ="+mqttObject.topic);
         if (mqttObject.topic == tempMoistureTopic)
@@ -37,41 +38,53 @@ public class mqttController : MonoBehaviour
             if(mqttObject.topic.Contains("moisture"))
             {
                 numValue = float.Parse(mqttObject.msg);
+                if(numValue > 200)
+                    numValue = 200;
                 Debug.Log("1");
-            // this.gameObject.transform.Find("MSG_MQTT").gameObject.GetComponent<TextMeshPro>().text = mqttObject.msg;
-            // Debug.Log(mqttObject.topic + " |||" + mqttObject.msg);
+
             }
                     
         }
 
         string tempTempTopic = topic.Substring(0,topic.Length-1);
-        tempMoistureTopic += "temperature";
+        tempTempTopic += "temperature"; //Check for temperature
         Debug.Log("Temp topic ="+tempTempTopic);
         Debug.Log("topic ="+mqttObject.topic);
         if (mqttObject.topic == tempTempTopic)
         {
-            if(mqttObject.topic.Contains("moisture"))
+            if(mqttObject.topic.Contains("temperature"))
             {
                 Debug.Log("2");
-                //this.gameObject.transform.Find("MSG_MQTT").gameObject.GetComponent<TextMeshPro>().text = mqttObject.msg;
-                //Debug.Log(mqttObject.topic + " |||" + mqttObject.msg);
+                string tempmsg = mqttObject.msg + " C";
+                this.gameObject.transform.Find("Canvas").gameObject.transform.Find("LCD-temp").GetComponent<TextMeshProUGUI>().text = tempmsg;
+                
+            }
+                    
+        }
+
+        string tempHumTopic = topic.Substring(0,topic.Length-1);
+        tempHumTopic += "humidity";    //Check for humidity
+        Debug.Log("Temp topic ="+tempHumTopic);
+        Debug.Log("topic ="+mqttObject.topic);
+        if (mqttObject.topic == tempHumTopic)
+        {
+            if(mqttObject.topic.Contains("humidity"))
+            {
+                Debug.Log("3");
+                string tempmsg = mqttObject.msg + " %";
+                this.gameObject.transform.Find("Canvas").gameObject.transform.Find("LCD-hum").gameObject.GetComponent<TextMeshProUGUI>().text = tempmsg;
+                
             }
                     
         }
     }
 
 
-    /*private void OnMessageArrivedHandler(string newMsg)
-    {
-        numValue = float.Parse(newMsg);
-        Debug.Log("Event Fired. The message, from Object " + nameController + " is = " + newMsg);
-    }*/
-
      private void Update()
     {
         float step = 0.5f * Time.deltaTime;
 
-        Vector3 rotationVector = new Vector3(objectToControl.transform.localEulerAngles.x, -numValue * 4.5f, objectToControl.transform.localEulerAngles.z);
+        Vector3 rotationVector = new Vector3(objectToControl.transform.localEulerAngles.x, -numValue * 0.9f, objectToControl.transform.localEulerAngles.z);
 
         objectToControl.transform.localRotation = Quaternion.Lerp(objectToControl.transform.localRotation, Quaternion.Euler(rotationVector), step);
 
